50709786d520d893d9570bfc84187d91a1672b80
Ingo Ruhnke <grumbel@gmail.com>
2023-06-13 16:35:13 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     libopencm3_src.url = "github:libopencm3/libopencm3";
@@ -15,7 +15,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages = flake-utils.lib.flattenTree rec {
+        packages = rec {
           libopencm3 = pkgs.stdenv.mkDerivation rec {
             pname = "libopencm3";
             version = "1.0.0-git";
